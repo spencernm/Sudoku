c42ffb82735918af2053fe69f1e733c3facc41a3
spencernm <spencerc10@nycstudents.net>
2026-04-12 18:24:31 -0400

    Update SudokuGenerator.java

--- SudokuGenerator.java
+++ SudokuGenerator.java
@@ -9,7 +9,7 @@ public class Sudoku {
         printBoard(board);
     }
     
-    public static ArrayList<Integer> generateShuffledList() {
+    public static ArrayList<Integer> generateShuffledList(){
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 1; i <= 9; i++) {
             list.add(i);
@@ -24,26 +24,53 @@ public class Sudoku {
         return list;
     }
     
-    public static void fillBoard(int[][] board, ArrayList<Integer> seedNums) {
-        for (int r = 0; r < 9; r++) {
-            int shift = (r * 3 + r / 3) % 9;
-            for (int c = 0; c < 9; c++) {
-                int index = (c + shift) % 9;
-                board[r][c] = seedNums.get(index);
+    public static boolean fillBoard(int[][] board, ArrayList<Integer> seedNums) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0){
+                    
+                    for (int num : seedNums) {
+                        if (isValid(board, row, col, num)){
+                            board[row][col] = num;
+                            
+                            if (fillBoard(board, seedNums)){
+                                return true;
+                            }
+                            else
+                            board[row][col] = 0; 
+                        }
+                    }
+                    return false;
+                }
             }
         }
+        return true;
+    }
+    
+    public static boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < 9; i++){
+            if (board[row][i] == num || board[i][col] == num){
+                return false;
+            }
+            int boxRow = 3*(row / 3) + i/3;
+            int boxCol = 3*(col / 3) + i%3;
+            if (board[boxRow][boxCol] == num){
+                return false;
+            }
+        }
+        return true;
     }
 
-    public static void printBoard(int[][] board) {
+    public static void printBoard(int[][] board){
         System.out.println("Solved Sudoku Generator");
         System.out.println("---------------------");
-        for (int r = 0; r < 9; r++) {
-            if (r > 0 && r % 3 == 0) {
+        for (int r = 0; r < 9; r++){
+            if (r > 0 && r % 3 == 0){
                 System.out.println("------+-------+------");
             }
             
-            for (int c = 0; c < 9; c++) {
-                if (c > 0 && c % 3 == 0) {
+            for (int c = 0; c < 9; c++){
+                if (c > 0 && c % 3 == 0){
                     System.out.print("| ");
                 }
                 System.out.print(board[r][c] + " ");
